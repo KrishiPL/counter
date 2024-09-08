cefd17f826bab92affc7c1f9854d5e335539f65f
KrishiPL <krishisivam1314@gmail.com>
2024-09-08 12:01:30 +0530

    Add files via upload
    Updated application with styles and UI

--- counter.js
+++ counter.js
@@ -1,19 +1,24 @@
 let counter = 0;
 
 const countIncrease = () => {
-  if(counter < 10){
-    counter = counter + 1;
+  if (counter < 10) {
+    counter += 1;
+    updateCounterDisplay();
   }
-   
 };
 
 const countDecrease = () => {
-  if(counter > 0){
-    counter = counter - 1;
+  if (counter > 0) {
+    counter -= 1;
+    updateCounterDisplay();
   }
 };
 
-
 const countReset = () => {
   counter = 0;
+  updateCounterDisplay();
+};
+
+const updateCounterDisplay = () => {
+  document.getElementById('counterValue').textContent = counter;
 };
